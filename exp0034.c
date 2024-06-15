89e7faaafcc9b9d14ca677f9f763fe8ca8846e93
Pradeep teja <pradeeptejapendurthy75@gmail.com>
2024-06-15 11:12:18 +0530

    Add files via upload

---
+++ exp0034.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+main()
+{
+    char grade;
+
+    printf("Enter Your Grade : ");
+    scanf("%c",&grade);
+
+    switch (grade)
+    {
+    case 'S':
+        printf("\n Super \n\n");
+        break;
+    case 'A':
+        printf("\n Very Good \n\n");
+        break;
+    case 'B':
+        printf("\n Fair \n\n");
+        break;
+    case 'Y':
+        printf("\n ABSCENT \n\n");
+        break;
+    case 'F':
+        printf("\n FAILES \n\n");
+        break;
+    }
+}
